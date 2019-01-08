71fe368479c07931d3d6c25dc2857eeb662b22c3
Vivek Myers <vivek.myers@gmail.com>
2019-01-07 19:29:17 -0800

    copied js basic bot to java one

--- bots/example_java/MyRobot.java
+++ bots/example_java/MyRobot.java
@@ -1,27 +1,26 @@
 package bc19;
 
 public class MyRobot extends BCAbstractRobot {
-	public int turn;
+    public int turn;
+    static int step = 0;
 
     public Action turn() {
-    	turn++;
-
-    	if (me.unit == SPECS.CASTLE) {
-    		if (turn == 1) {
-    			log("Building a pilgrim.");
-    			return buildUnit(SPECS.PILGRIM,1,0);
-    		}
-    	}
-
-    	if (me.unit == SPECS.PILGRIM) {
-    		if (turn == 1) {
-    			log("I am a pilgrim.");
-                 
-                //log(Integer.toString([0][getVisibleRobots()[0].castle_talk]));
-    		}
-    	}
-
-    	return null;
-
-	}
+        turn++;
+        if (me.unit == SPECS.CASTLE) {
+            if (step++ % 10 == 0) {
+                return buildUnit(SPECS.CRUSADER, 1, 1);
+            }
+        }
+        if (me.unit == SPECS.CRUSADER) {
+            int i = 0, j = 0;
+            do {
+                i = (int) (Math.random() * 3);
+                j = (int) (Math.random() * 3);
+            } while (i == 0 && j == 0);
+            i--;
+            j--;
+            return move(i, j);
+        }
+        return null;
+    }
 }
