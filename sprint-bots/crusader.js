a98b1223003c78af3384614747c6174c24381a51
Mihir <mihir.v.patel7@gmail.com>
2019-01-10 16:56:57 -0800

    comments

--- sprint-bots/crusader.js
+++ sprint-bots/crusader.js
@@ -11,15 +11,16 @@ export function Crusader() {
 function crusaderTurn() {
     // attack code
     for (let i of this.getVisibleRobots()) {
-        if (this.fuel > 20 &&
-            i.team != this.me.team && this.dist([i.x, i.y], [this.me.x, this.me.y]) <= 4)
+        if (this.fuel > 20 && i.team != this.me.team && this.dist([i.x, i.y], [this.me.x, this.me.y]) <= 4)
             return this.attack(i.x - this.me.x, i.y - this.me.y);
     }
+
+    // movement code
     let route = this.path(this.target);
     if (this.fuel > 5) {
-        if (route.length) {
+        if (route.length > 0) { //A* towards target
             return this.move(...route[0]);
-        } else if (this.fuel > 5) {
+        } else { //random move
             return this.move(...this.randomMove());
         }
     }
--- sprint-bots/prophet.js
+++ sprint-bots/prophet.js
@@ -9,17 +9,20 @@ export function Prophet() {
  * March across map reflection.
  */
 function prophetTurn() {
+
+    // attack code
     for (let i of this.getVisibleRobots()) {
-        if (this.fuel > 20 &&
-            i.team != this.me.team && this.dist([i.x, i.y], [this.me.x, this.me.y]) <= 8
+        if (this.fuel > 20 && i.team != this.me.team && this.dist([i.x, i.y], [this.me.x, this.me.y]) <= 8
                                    && this.dist([i.x, i.y], [this.me.x, this.me.y]) >= 4)
             return this.attack(i.x - this.me.x, i.y - this.me.y);
     }
+
+    // movement code
     let route = this.path(this.target);
     if (this.fuel > 5) {
-        if (route.length) {
+        if (route.length > 0) { //A* towards target
             return this.move(...route[0]);
-        } else if (this.fuel > 5) {
+        } else { //random move
             return this.move(...this.randomMove());
         }
     }
