28c3f83de25ea9cb1e4c5083b068314f7a465fe7
Vivek Myers <vivek.myers@gmail.com>
2019-01-11 23:41:56 -0800

    Start with Pilgrim build

--- new-bots/castle.js
+++ new-bots/castle.js
@@ -3,13 +3,14 @@ import {SPECS} from 'battlecode';
 export function Castle() {
     this.turn = castleTurn;
     this.step = 0;
+    this.starting = true;
 }
 
 function castleTurn() {
     this.step++;
     let choice = this.randomMove();
 
-    // mage spawn 
+    // mage spawn
     /* if (this.fuel >= 50 && this.karbonite >= 30 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
         return this.buildUnit(SPECS.PREACHER, choice[0], choice[1]);
     }
@@ -18,28 +19,30 @@ function castleTurn() {
     } */
 
     // base spawn rate
-    if (Math.random() < 0.5)
+    if (Math.random() < 0.2 || this.starting) {
         if (this.fuel >= 50 && this.karbonite >= 10 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
+            this.starting = false;
             return this.buildUnit(SPECS.PILGRIM, choice[0], choice[1]);
         } else {
             return;
         }
-    else if (Math.random() < 1.0 / 3.0)
+    } else if (Math.random() < 1.0 / 3.0) {
         if (this.fuel >= 50 && this.karbonite >= 20 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             return this.buildUnit(SPECS.CRUSADER, choice[0], choice[1]);
         } else {
             return;
         }
-    else if (Math.random() < 0.5)
+    } else if (Math.random() < 0.5) {
         if (this.fuel >= 50 && this.karbonite >= 25 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             return this.buildUnit(SPECS.PROPHET, choice[0], choice[1]);
         } else {
             return;
         }
-    else
+    } else {
         if (this.fuel >= 50 && this.karbonite >= 30 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             return this.buildUnit(SPECS.PREACHER, choice[0], choice[1]);
         } else {
             return;
         }
+    }
 }
