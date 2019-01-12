aa224102c0032c2c16bbf14c82c4a90d2b005c69
Mihir <mihir.v.patel7@gmail.com>
2019-01-11 18:39:47 -0800

    repairing merge conflicts and branch issues

---
+++ sprint-bots/crusader.js
@@ -0,0 +1,49 @@
+import {SPECS} from 'battlecode';
+
+export function Crusader() {
+    this.turn = crusaderTurn;
+    this.target = this.reflection();
+}
+
+/**
+ * March across map reflection.
+ */
+function crusaderTurn() {
+    // attack code
+    for (let i of this.getVisibleRobots()) {
+        if (this.fuel > 20) {
+            if (i.team != this.me.team && this.dist([i.x, i.y], [this.me.x, this.me.y]) <= 4) {
+                return this.attack(i.x - this.me.x, i.y - this.me.y);
+            }
+        } else return;
+    }
+    if (this.me.x == this.target[0] && this.me.y == this.target[1]) {
+         let r = () => [Math.floor(Math.random() * this.map[0].length),
+                         Math.floor(Math.random() * this.map.length)];
+         this.target = r();
+         while (!this.map[this.target[1]][this.target[0]]) {
+             this.target = r();
+         }
+    }
+
+    // movement code
+    let route = this.path(this.target);
+    if (this.fuel > 10) {
+        if (route.length > 0) { //A* towards target
+            return this.move(...route[0]);
+        } else { //random move
+            return this.move(...this.randomMove());
+        }
+    }
+/* Ignore. For timing tests with BFS
+    let move = this.bfs(this.target);
+    if (this.fuel > 10) {
+        if (move[0]!=0 || move[1]!=0) { //A* towards target
+            //this.log("Move: "+route[0]);
+            return this.move(...move);
+        } else { //random move
+            this.log("No route detected");
+            return this.move(...this.randomMove());
+        }
+    } */
+}
