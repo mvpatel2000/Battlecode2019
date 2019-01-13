6fb74c018c98f30a264d8293280db5c6bba7c1d7
Vivek Myers <vivek.myers@gmail.com>
2019-01-12 22:19:26 -0800

    bounds checking bugfix

--- new-bots/castle.js
+++ new-bots/castle.js
@@ -9,7 +9,7 @@ export function Castle() {
 function castleTurn() {
     this.step++;
     let choice = this.randomMove();
-
+    const adj = this.turn > 10 ? 50 : 0;
     // mage spawn
     /* if (this.fuel >= 50 && this.karbonite >= 30 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
         return this.buildUnit(SPECS.PREACHER, choice[0], choice[1]);
@@ -20,26 +20,26 @@ function castleTurn() {
 
     // base spawn rate
     if (this.starting || (Math.random() < 0.2)) {
-        if (this.fuel >= 50 && this.karbonite >= 10 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
+        if (this.fuel >= 50 + adj && this.karbonite >= 10 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             this.starting = false;
             return this.buildUnit(SPECS.PILGRIM, choice[0], choice[1]);
         } else {
             return;
         }
     } else if (Math.random() < 1.0 / 3.0) {
-        if (this.fuel >= 50 && this.karbonite >= 20 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
+        if (this.fuel >= 50 + adj && this.karbonite >= 20 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             return this.buildUnit(SPECS.CRUSADER, choice[0], choice[1]);
         } else {
             return;
         }
     } else if (Math.random() < 0.5) {
-        if (this.fuel >= 50 && this.karbonite >= 25 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
+        if (this.fuel >= 50 + adj && this.karbonite >= 25 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             return this.buildUnit(SPECS.PROPHET, choice[0], choice[1]);
         } else {
             return;
         }
     } else {
-        if (this.fuel >= 50 && this.karbonite >= 30 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
+        if (this.fuel >= 50 + adj && this.karbonite >= 30 && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])) {
             return this.buildUnit(SPECS.PREACHER, choice[0], choice[1]);
         } else {
             return;
