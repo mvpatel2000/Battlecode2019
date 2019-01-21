6fc37f825d4b9d19c9946acc305d8ef5b83b4a78
Vivek Myers <vivek.myers@gmail.com>
2019-01-20 16:04:43 -0800

    prophet harassment planning

--- prophet-harass/prophet.js
+++ prophet-harass/prophet.js
@@ -9,6 +9,26 @@ export function Prophet() {
     this.target = this.enemyCastleLocations[this.targetCtr];
     this.step = 0;
 
+    let sig = this.spawnPoint.signal;
+    if (sig >> 15) {
+        this.harasser = true;
+        this.avoid = [((1 << 5) - 1) & (sig >> 10),
+                        ((1 << 5) - 1) & (sig >> 5),
+                        ((1 << 5) - 1) & sig]
+    }
+
+    //harassers
+    if (this.harasser) {
+        this.resourceClusters = this.clusterResourceTiles();
+        this.resourceCentroids = this.resourceClusters.map(x => this.centroid(x));
+        this.avoid.forEach(i => {
+            this.resourceCentroids.splice(i, 1, null);
+        });
+        this.resourceCentroids = this.resourceCentroids.filter(i => i);
+        this.queue = this.resourceCentroids;
+        this.turn = harassTurn;
+    }
+
     //determine spawn castle for grid
     this.spawnPoint = this.getVisibleRobots().filter(i => i.unit < 2 && this.distSquared([i.x, i.y], [this.me.x, this.me.y]) <= 2 && i.signal >= 0)[0];
     this.target = this.decodeExactLocation(this.spawnPoint.signal);
@@ -47,7 +67,7 @@ function prophetTurn() {
         } else {
             return this.move(...[finmove[0] - this.me.x, finmove[1] - this.me.y]);
         }
-    }  
+    }
 
     //get nearest grid square
     //move to it
@@ -62,7 +82,7 @@ function prophetTurn() {
             return this.move(...route[0]);
         }
     }
-    
+
     /*
     while (this.me.x == this.target[0] && this.me.y == this.target[1]) { //reset target if meet it
         if (this.targetCtr < this.enemyCastleLocations.length) {
@@ -91,4 +111,18 @@ function shouldRun() {
             .map(i => this.distSquared([this.me.x, this.me.y], [i.x, i.y]))
             .map(i => i < 18)
             .reduce((a, b) => a || b, false);
+}
+
+
+function harassTurn() {
+    let attackbot = this.getRobotToAttack();
+    if (attackbot && !shouldRun.call(this)) {
+        if (this.fuel > SPECS.UNITS[this.me.unit].ATTACK_FUEL_COST) {
+            return this.attack(attackbot.x - this.me.x, attackbot.y - this.me.y);
+        }
+    }
+    if (this.dist(this.queue[0], [this.me.x, this.me.y]) < 3) {
+        this.queue.push(this.queue.shift());
+    }
+    return this.go(this.queue[0]);
 }
