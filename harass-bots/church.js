c642758087b52dd5efa246d24b949a90642064f1
Mihir <mihir.v.patel7@gmail.com>
2019-01-30 15:55:10 -0800

    spawn fix

--- harass-bots/church.js
+++ harass-bots/church.js
@@ -17,6 +17,7 @@ export function Church() {
     this.myClusterIndex = this.findNearestClusterIndex([this.me.x, this.me.y], this.resourceClusters);
 
     this.aggroChurch = false;
+    this.closestThreat = [0,0];
 
     if (broadcastingPilgrims.length > 0 && zeroBroadcast.length == 0) { //it is a mission or aggro church!
 
@@ -98,16 +99,15 @@ function churchTurn() {
         if (threats.length > 0 || this.extendedDefenseTimer > 0) { // attacking threat
             if (this.karbonite >= 30 && this.fuel >= 50) {
                 let minDist = 7939;
-                let closestThreat = [-100,-100];
                 for (let k = 0; k < threats.length; k++) {
                     let dist = this.distSquared([this.me.x, this.me.y], [threats[k].x, threats[k].y]);
                     if (dist < minDist) {
                         minDist = dist;
-                        closestThreat = [threats[k].x, threats[k].y];
+                        this.closestThreat = [threats[k].x, threats[k].y];
                     }
                 }
                 if (prophetThreats.length == 0 && threats.length > 0) { //build preachers unless you see 2 prophets
-                    let choice = this.getSpawnLocation(closestThreat[0], closestThreat[1]);
+                    let choice = this.getSpawnLocation(this.closestThreat[0], this.closestThreat[1]);
                     if (choice != null) {
                         if (this.defensePositions.length > 0) {
                             let sub = (a, b) => [a[0] - b[0], a[1] - b[1]];
@@ -115,7 +115,7 @@ function churchTurn() {
                                 (v[0] * w[0] + v[1] * w[1]) / (Math.sqrt(v[0] * v[0] + v[1] * v[1])
                                     * Math.sqrt(w[0] * w[0] + w[1] * w[1])));
                             let candidates = this.defensePositions.filter(i =>
-                                angle(sub(i, this.pos()), sub(closestThreat, this.pos())) <= Math.PI / 4)
+                                angle(sub(i, this.pos()), sub(this.closestThreat, this.pos())) <= Math.PI / 4)
                             let defenseTarget = candidates[0];
                             for (let i = 0; i < this.defensePositions.length; i++) {
                                 if (this.arrEq(this.defensePositions[i], defenseTarget)) {
@@ -134,7 +134,7 @@ function churchTurn() {
                     }
                 }
                 else {
-                    let choice = this.getSpawnLocation(closestThreat[0], closestThreat[1]);
+                    let choice = this.getSpawnLocation(this.closestThreat[0], this.closestThreat[1]);
                     if (choice != null) {
                         if (this.defensePositions.length > 0) {
                             let sub = (a, b) => [a[0] - b[0], a[1] - b[1]];
@@ -142,7 +142,7 @@ function churchTurn() {
                                 (v[0] * w[0] + v[1] * w[1]) / (Math.sqrt(v[0] * v[0] + v[1] * v[1])
                                     * Math.sqrt(w[0] * w[0] + w[1] * w[1])));
                             let candidates = this.defensePositions.filter(i =>
-                                angle(sub(i, this.pos()), sub(closestThreat, this.pos())) <= Math.PI / 4)
+                                angle(sub(i, this.pos()), sub(this.closestThreat, this.pos())) <= Math.PI / 4)
                             let defenseTarget = candidates[0];
                             for (let i = 0; i < this.defensePositions.length; i++) {
                                 if (this.arrEq(this.defensePositions[i], defenseTarget)) {
@@ -169,8 +169,8 @@ function churchTurn() {
         }
         else if (this.karbonite >= 25 && this.fuel >= 50
             && this.distSquared([visibleEnemies[0].x, visibleEnemies[0].y],[this.me.x, this.me.y]) >= 25) {
-            let closestThreat = [visibleEnemies[0].x, visibleEnemies[0].y]
-            let choice = this.getSpawnLocation(closestThreat[0], closestThreat[1]);
+            this.closestThreat = [visibleEnemies[0].x, visibleEnemies[0].y]
+            let choice = this.getSpawnLocation(this.closestThreat[0], this.closestThreat[1]);
             if (choice != null) {
                 if (this.defensePositions.length > 0) {
                     let sub = (a, b) => [a[0] - b[0], a[1] - b[1]];
@@ -178,7 +178,7 @@ function churchTurn() {
                         (v[0] * w[0] + v[1] * w[1]) / (Math.sqrt(v[0] * v[0] + v[1] * v[1])
                             * Math.sqrt(w[0] * w[0] + w[1] * w[1])));
                     let candidates = this.defensePositions.filter(i =>
-                        angle(sub(i, this.pos()), sub(closestThreat, this.pos())) <= Math.PI / 4)
+                        angle(sub(i, this.pos()), sub(this.closestThreat, this.pos())) <= Math.PI / 4)
                     let defenseTarget = candidates[0];
                     for (let i = 0; i < this.defensePositions.length; i++) {
                         if (this.arrEq(this.defensePositions[i], defenseTarget)) {
@@ -194,8 +194,8 @@ function churchTurn() {
             }
         }
         else if (this.karbonite >= 15 && this.fuel >= 50) {
-            let closestThreat = [visibleEnemies[0].x, visibleEnemies[0].y]
-            let choice = this.getSpawnLocation(closestThreat[0], closestThreat[1]);
+            this.closestThreat = [visibleEnemies[0].x, visibleEnemies[0].y]
+            let choice = this.getSpawnLocation(this.closestThreat[0], this.closestThreat[1]);
             if (choice != null) {
                 if (this.defensePositions.length > 0) {
                     let sub = (a, b) => [a[0] - b[0], a[1] - b[1]];
