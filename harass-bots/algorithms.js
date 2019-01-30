c2153d5c3422ff55583fe8c60cdbe59702f5326a
Mihir <mihir.v.patel7@gmail.com>
2019-01-30 01:20:48 -0800

    fix churches

--- harass-bots/algorithms.js
+++ harass-bots/algorithms.js
@@ -799,22 +799,19 @@ export const Algorithms = (function() {
             const choices = [[1,0], [1,1], [0,1], [-1,1], [-1,0], [-1,-1], [0,-1], [1,-1]];
             let optimalIndex = Math.floor((4*(Math.atan2(y-this.me.y,x-this.me.x)/Math.PI))+8.5) % 8;
             let choice = choices[optimalIndex];
-            let matrixmod = (this.originalBaseLocation[0] + this.originalBaseLocation[1]) % 2;
-            if ( (this.me.x + choice[0] + this.me.y + choice[1]) % 2 == matrixmod
-                && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])
+            //let matrixmod = (this.originalBaseLocation[0] + this.originalBaseLocation[1]) % 2;
+            if (!this.occupied(this.me.x + choice[0], this.me.y + choice[1])
                 && !this.karbonite_map[this.me.y + choice[1]][this.me.x + choice[0]]
                 && !this.fuel_map[this.me.y + choice[1]][this.me.x + choice[0]])
                 return choice;
             for (let i = 1; i <= 4; i++) {
                 choice = choices[(optimalIndex + 1) % 8];
-                if ((this.me.x + choice[0] + this.me.y + choice[1]) % 2 == matrixmod
-                    && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])
+                if (!this.occupied(this.me.x + choice[0], this.me.y + choice[1])
                     && !this.karbonite_map[this.me.y + choice[1]][this.me.x + choice[0]]
                     && !this.fuel_map[this.me.y + choice[1]][this.me.x + choice[0]])
                     return choice;
                 choice = choices[(optimalIndex+(8 - i)) % 8];
-                if ((this.me.x + choice[0] + this.me.y + choice[1]) % 2 == matrixmod
-                    && !this.occupied(this.me.x + choice[0], this.me.y + choice[1])
+                if (!this.occupied(this.me.x + choice[0], this.me.y + choice[1])
                     && !this.karbonite_map[this.me.y + choice[1]][this.me.x + choice[0]]
                     && !this.fuel_map[this.me.y + choice[1]][this.me.x + choice[0]])
                     return choice;
